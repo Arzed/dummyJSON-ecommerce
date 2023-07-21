a1af9d5f6308e653da778fa9a278be171fc747cc
Arzed <132367824+Arzed@users.noreply.github.com>
2023-07-21 17:09:50 +0700

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,18 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google"
 
-export const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-  ],
-  pages: {
-    signIn: "/auth/signin"
-  }
-});
+/* You shouldn't export authOptions in API route.ts / route.js file.
+ This is the cause of error!! */
 
-export { handler as GET, handler as POST };
+export const authOptions: AuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+        }),
+    ],
+}
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST}
