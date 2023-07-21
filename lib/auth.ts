d0a1fc629c7fa8ac55f2f0923e9fcf937dca6ba5
Arzed <132367824+Arzed@users.noreply.github.com>
2023-07-21 18:32:09 +0700

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,7 +5,7 @@ export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
         clientId: process.env.GOOGLE_ID as string,
-        clientSecret: process.env.GOOGLE_SECRET_KEY as string
+        clientSecret: process.env.GOOGLE_SECRET as string
     })
   ]
 };
