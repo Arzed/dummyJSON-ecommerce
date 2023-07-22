0daeff2e2539ea96873cb5332f5dd265c5ca2df8
Arzed <tosoon149@gmail.com>
2023-07-22 17:38:42 +0700

    second update

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -1,14 +1,22 @@
 import { Navbar } from "@/components/Navbar"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import axios from "axios";
 
 interface RootLayoutProps {
   children: React.ReactNode
 }
 
-export default function ProductLayout({ children }: RootLayoutProps) {
+async function getCategories() {
+  const res = await axios.get(`https://dummyjson.com/products/categories`).then(res => res.data).catch(err => console.error(err))
+  
+  return res;
+}
+
+export default async function ProductLayout({ children }: RootLayoutProps) {
+  const categories = await getCategories();
   return (
     <div className="relative flex flex-col">
-      <Navbar />
+      <Navbar categories={categories} />
       <div className='flex'>
           {children}
       </div>
